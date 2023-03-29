8b4c061b2479c66f6c7a65f6935c6bd8d9f753d9
meitamputri <meitamelaniputri@smkwikrama.sch.id>
2023-03-29 15:58:10 +0700

    diva

---
+++ resources/views/pages/admin/petugas/create.blade.php
@@ -0,0 +1,28 @@
+<form action="{{ route('petugas.store') }}" method="POST" enctype="multipart/form-data">
+    @csrf
+    <label for="">Nama</label><br>
+    <input class="form-control mb-3" type="text" name="name_petugas">
+    <label for="">Username</label>
+    <input class="form-control mb-3" type="text" name="username">
+    <label for="">Email</label>
+    <input class="form-control mb-3" type="text" name="email">
+    <label for="">Alamat</label>
+    <input class="form-control mb-3" type="text" name="alamat">
+    <label for="">Jenis Kelamin</label>
+    <select name="jeniskelamin" id="jeniskelamin" class="form-control mb-3">
+        <option value="jeniskelamin" selected>Jenis Kelamin</option>
+        <option value="laki-laki">laki-Laki</option>
+        <option value="perempuan">perempuan</option>
+    </select>
+    <label for="">Password</label>
+    <input class="form-control mb-3" type="password" name="password">
+    <label for="">No Telp.</label>
+    <input class="form-control mb-3" type="number" pattern="\(\d\d\d\)-\d\d\d\d\d\d\d\d" name="no_telp" placeholder="(+62) 999999999" required />
+    <label for="">Level</label>
+    <select name="level" id="level" class="form-control mb-3">
+        <option value="petugas" selected>Pilih Level (Default Petugas)</option>
+        <option value="admin">Admin</option>
+        <option value="petugas">Petugas</option>
+    </select>
+    <button class="btn btn-primary mb-3" type="submit">submit</button>
+</form>
---
+++ resources/views/pages/admin/petugas/edit.blade.php
@@ -0,0 +1,28 @@
+<form action="{{ route('petugas.update', $edit->id) }}" method="POST" enctype="multipart/form-data">
+    @method('PUT')
+    @csrf
+    <label for="">Name</label><br>
+    <input class="form-control mb-3" type="text" name="name_petugas" value="{{ $edit->name_petugas }}">
+    <label for="">Username</label>
+    <input class="form-control mb-3" type="text" name="username" value="{{ $edit->name_petugas }}">
+    <label for="">Alamat</label>
+    <input class="form-control mb-3" type="text" name="alamat">
+    <label for="">Jenis Kelamin</label>
+    <select name="jeniskelamin" id="jeniskelamin" class="form-control mb-3">
+        <option value="jeniskelamin" selected>Jenis Kelamin</option>
+        <option value="laki-laki">laki-Laki</option>
+        <option value="perempuan">perempuan</option>
+    </select>
+    <label for="">Password</label>
+    <input class="form-control mb-3" type="password" name="password" value="{{ $edit->name_petugas }}">
+    <label for="">No Telp.</label>
+    <input class="form-control mb-3" type="number" pattern="\(\d\d\d\)-\d\d\d\d\d\d\d\d" name="no_telp" placeholder="(+62) 999999999" required value="{{ $edit->no_telp }}" />
+    <label for="">Level</label>
+    <select name="level" id="level" class="form-control mb-3">
+        <option selected>{{ $edit->level }}</option>
+        <option value="admin">Admin</option>
+        <option value="petugas">Petugas</option>
+    </select>
+    <a href="{{ route('petugas.index') }}" class="btn btn-secondary mb-3">Cancel</a>
+    <button class="btn btn-warning mb-3" type="submit">Update</button>
+</form>
